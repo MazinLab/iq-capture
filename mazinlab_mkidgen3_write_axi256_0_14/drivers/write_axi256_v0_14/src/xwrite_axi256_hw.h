b9883acaad3c1f43b61dd4f9bad7fdc70b5891a0
jebbailey <baileyji@ucsb.edu>
2022-01-17 13:10:04 -0800

    add another attempt

---
+++ mazinlab_mkidgen3_write_axi256_0_14/drivers/write_axi256_v0_14/src/xwrite_axi256_hw.h
@@ -0,0 +1,43 @@
+// ==============================================================
+// Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2021.1 (64-bit)
+// Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+// ==============================================================
+// control
+// 0x00 : Control signals
+//        bit 0  - ap_start (Read/Write/COH)
+//        bit 1  - ap_done (Read/COR)
+//        bit 2  - ap_idle (Read)
+//        bit 3  - ap_ready (Read/COR)
+//        bit 7  - auto_restart (Read/Write)
+//        others - reserved
+// 0x04 : Global Interrupt Enable Register
+//        bit 0  - Global Interrupt Enable (Read/Write)
+//        others - reserved
+// 0x08 : IP Interrupt Enable Register (Read/Write)
+//        bit 0  - enable ap_done interrupt (Read/Write)
+//        bit 1  - enable ap_ready interrupt (Read/Write)
+//        others - reserved
+// 0x0c : IP Interrupt Status Register (Read/TOW)
+//        bit 0  - ap_done (COR/TOW)
+//        bit 1  - ap_ready (COR/TOW)
+//        others - reserved
+// 0x10 : Data signal of capturesize
+//        bit 26~0 - capturesize[26:0] (Read/Write)
+//        others   - reserved
+// 0x14 : reserved
+// 0x18 : Data signal of out_r
+//        bit 31~0 - out_r[31:0] (Read/Write)
+// 0x1c : Data signal of out_r
+//        bit 31~0 - out_r[63:32] (Read/Write)
+// 0x20 : reserved
+// (SC = Self Clear, COR = Clear on Read, TOW = Toggle on Write, COH = Clear on Handshake)
+
+#define XWRITE_AXI256_CONTROL_ADDR_AP_CTRL          0x00
+#define XWRITE_AXI256_CONTROL_ADDR_GIE              0x04
+#define XWRITE_AXI256_CONTROL_ADDR_IER              0x08
+#define XWRITE_AXI256_CONTROL_ADDR_ISR              0x0c
+#define XWRITE_AXI256_CONTROL_ADDR_CAPTURESIZE_DATA 0x10
+#define XWRITE_AXI256_CONTROL_BITS_CAPTURESIZE_DATA 27
+#define XWRITE_AXI256_CONTROL_ADDR_OUT_R_DATA       0x18
+#define XWRITE_AXI256_CONTROL_BITS_OUT_R_DATA       64
+
